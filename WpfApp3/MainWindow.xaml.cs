94f88c7628d20fd0921ed6e0481b6b72a0d6fa3a
heliconia8 <d.samokvasova@gmail.com>
2023-03-16 11:17:45 +0400

    Добавьте файлы проекта.

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using QRCoder;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tb_generate_Click(object sender, RoutedEventArgs e)
+        {
+            QRCodeGenerator qrGenerator = new();
+            QRCodeData qRCodeData = qrGenerator.CreateQrCode(tb_content.Text, QRCodeGenerator.ECCLevel.Q);
+
+            QRCode qR = new QRCode(qRCodeData);
+
+            Bitmap qr = qR.GetGraphic(150);
+
+            image_qrcode.Source = Convert(qr);
+
+
+        }
+        public BitmapImage Convert(Bitmap src)
+        {
+            MemoryStream ms = new MemoryStream();
+            ((System.Drawing.Bitmap)src).Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();    
+            return image;
+        }
+    }
+}
